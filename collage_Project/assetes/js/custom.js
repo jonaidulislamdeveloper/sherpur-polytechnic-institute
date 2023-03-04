f5bf77b8351af3f81d78eea520e1bebfaa99ddec
jonaidulislamdeveloper <126936942+jonaidulislamdeveloper@users.noreply.github.com>
2023-03-04 10:45:48 +0600

    Add files via upload

---
+++ collage_Project/assetes/js/custom.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+
+//header_slider
+    $(".header_slider_section .slider").owlCarousel({
+        items:1,
+        loop:true,
+        nav:true,
+        autoplay:true,
+        autoplaySpeed:2000,
+    });
+
+    //header_slider_class_add
+    $(".owl-nav").addClass("header-slider-nav");
+
+
+
+//magnificPopup
+
+$('.gallerys').magnificPopup({
+    type: 'image',
+    delegate: 'a',
+    gallery: {
+        enabled: true
+      }
+  });
+
+
+//teacher-sign-up
+
+$('.close_btn').click()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  });
